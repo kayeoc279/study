9f95846a3220806c1c9ba1c83088b35d937232bb
I-Cheng Cheng <i-cheng.cheng@sifive.com>
2020-04-19 10:00:27 +0000

    * Add cpp clang-format config file * inplace fix format

--- cpp/exception/main.cpp
+++ cpp/exception/main.cpp
@@ -3,41 +3,38 @@
 
 using namespace std;
 
-
 class Tmp {
-
-
 public:
-    string myname; 
-    Tmp(){
+    string myname;
+    Tmp()
+    {
         cout << "tmp is create" << endl;
         myname = "string 123";
     }
 };
 
-void do_except(int i){
-    if( i == 0 )
+void do_except(int i)
+{
+    if (i == 0)
         // This line will new a Tmp class object
         throw Tmp();
-    else{
+    else {
         cout << "before  i = " << i << endl;
-        do_except(i-1);
+        do_except(i - 1);
         // This will not execute Due to throw in previos line.
         cout << "after   i = " << i << endl;
     }
 }
 
-
-int main(){
-
-    try{
-        do_except( 5 );
+int main()
+{
+    try {
+        do_except(5);
     }
-    catch( int e ){
+    catch (int e) {
         cout << "error catch " << e << endl;
     }
-    catch( Tmp e ){
+    catch (Tmp e) {
         cout << "error catch Tmp " << e.myname << endl;
     }
-   
 }
--- cpp/exception/run.sh
+++ cpp/exception/run.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+clang-format -i *.cpp -style=file
+
 g++ main.cpp -o main
 
 ./main
